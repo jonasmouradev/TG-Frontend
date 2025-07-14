64fb3d5d1c6671ecb1a1845cf56f165ec3128633
Jonas Moura <jonasmouradev@gmail.com>
2025-07-14 16:28:53 -0300

    feat: :construction: updating sidebar

--- src/shared/components/layout/Navbar/index.tsx
+++ src/shared/components/layout/Navbar/index.tsx
@@ -1,7 +1,8 @@
 import useUserContext from '@/shared/contexts/UserContext';
-import { House, Activity, Search, NotebookText, Settings } from 'lucide-react';
+import { House, Activity, Search, NotebookText, Settings, Inbox, ChartColumn, Plus, Moon } from 'lucide-react';
 import { ElementType } from 'react';
 import { useLocation, useNavigate } from 'react-router';
+import { Button } from '../../ui';
 
 const Navbar = () => {
   const user = useUserContext();
@@ -32,40 +33,71 @@ const Navbar = () => {
       icon: NotebookText,
     },
     {
+      label: 'Caixa de Entrada',
+      path: '/home',
+      icon: Inbox,
+    },
+    {
+      label: 'Estatísticas',
+      path: '/home',
+      icon: ChartColumn,
+    },
+    {
       label: 'Configurações',
       path: '/configs',
       icon: Settings,
     },
   ];
 
+  const Header = () => (
+    <header className="h-48 w-72 flex flex-col justify-evenly font-sans items-center">
+      <h1 className="text-3xl text-zinc-50 self-start pl-6">Seleto Inc</h1>
+      <div className="relative w-full px-4">
+        <Search className="absolute left-6 top-1/2 transform -translate-y-1/2 text-zinc-400 w-4 h-4" />
+        <input
+          type="search"
+          placeholder="Pesquisar..."
+          className="w-full h-8 pl-10 pr-3 py-2 rounded-md border-2 border-zinc-900 bg-transparent text-zinc-50 placeholder-zinc-400 text-sm &:focus"
+        />
+      </div>
+    </header>
+  );
+
+  const MenuItems = () => (
+    <>
+      <ul className="h-auto flex flex-col items-center text-zinc-50 p-4">
+        {navigateList.map((item, index) => (
+          <li
+            onClick={() => handleNavigation(item.path)}
+            key={index}
+            className="flex w-full text-center items-center px-4 py-3 hover:bg-zinc-700 transition-colors gap-3 rounded-sm text-zinc-200"
+          >
+            {item.icon && <item.icon size={18} />}
+            <a className="flex items-center justify-center text-sm font-medium">{item.label}</a>
+          </li>
+        ))}
+      </ul>
+      <div className="relative w-full px-4">
+        <Plus className="absolute left-10 top-1/2 transform -translate-y-1/2 text-white w-4 h-4" />
+        <Button className="bg-blue-500 w-full">Criar uma vaga</Button>
+      </div>
+    </>
+  );
+
+  const Footer = () => (
+    <footer className="flex justify-between px-8 bg-transparent border-t-2 border-zinc-900 mt-auto py-6">
+      <h1 className="text-zinc-400">Tema</h1>
+      <Moon className="text-zinc-400" />
+    </footer>
+  );
+
   return (
     <>
       {user.id && (
-        <nav className="w-72 h-screen bg-zinc-800 fixed top-0 left-0 z-10 cursor-pointer shadow-md">
-          <div className="h-48 w-72 flex flex-col justify-evenly font-sans items-center">
-            <h1 className="text-3xl text-zinc-50">Seleto Inc</h1>
-            <div className="relative w-3/4">
-              <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-zinc-400 w-4 h-4" />
-              <input
-                type="search"
-                placeholder="Search..."
-                className="w-full h-8 pl-10 pr-3 py-2 rounded-md bg-zinc-700 text-zinc-50 placeholder-zinc-400"
-              />
-            </div>
-          </div>
-          <h1 className="text-zinc-400 px-8">Menu Principal</h1>
-          <ul className="h-full flex flex-col items-center text-zinc-50 p-4">
-            {navigateList.map((item, index) => (
-              <li
-                onClick={() => handleNavigation(item.path)}
-                key={index}
-                className="flex w-full text-center items-center px-4 py-3 hover:bg-zinc-700 transition-colors gap-3 rounded-sm text-white"
-              >
-                {item.icon && <item.icon size={18} />}
-                <a className="flex items-center justify-center text-sm font-medium">{item.label}</a>
-              </li>
-            ))}
-          </ul>
+        <nav className="flex flex-col w-72 h-screen bg-transparent border-r-2 border-zinc-900 fixed top-0 left-0 z-10 cursor-pointer shadow-md">
+          <Header />
+          <MenuItems />
+          <Footer />
         </nav>
       )}
     </>
